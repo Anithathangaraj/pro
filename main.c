66f43a42c61d0a753dcebde1107ba7b78666488e
Anithathangaraj <38176220+Anithathangaraj@users.noreply.github.com>
2018-05-01 11:18:04 +0530

    create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int N;
+scanf("%d",N);
+while(1<=N<=100000)
+if(1<=N<=100000)
+{
+printf("positive");
+}
+ifelse(N==0)
+{
+printf("zero");
+}
+else
+{
+printf("negative");
+}
+}
+return 0;
+}
