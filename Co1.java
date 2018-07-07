ea0c615d8c6e77a4556eef149e5c3a5ceabcb21d
Anithathangaraj <38176220+Anithathangaraj@users.noreply.github.com>
2018-07-07 15:07:29 +0530

    Update Co1

--- Co1.java
+++ Co1.java
@@ -3,9 +3,10 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner k=new Scanner(System.in);
-int s=k.nextInt();int fact=1;
-		for(int i=1;i<=s;i++){
-		    fact=fact*i;
+int s=k.nextInt();int fact=1,j=1;
+		for(int i=0;i<s;i++){
+		    fact=fact*j;
+			j++;
 		}
 		System.out.print(fact);
 	}
