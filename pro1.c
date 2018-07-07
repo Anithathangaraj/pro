3fcadd57efad5d8829980b88a5ba1d3a0d98f21f
Anithathangaraj <38176220+Anithathangaraj@users.noreply.github.com>
2018-07-07 12:26:32 +0530

    Update pro1.c

--- pro1.c
+++ pro1.c
@@ -3,11 +3,19 @@ int main()
 {
 int n;
 scanf("%d",&n);
-if(n%2!=0)
-{
-printf("ODD");
-}
-else
-{
-printf("EVEN");
+  if((n>=1)&&(n<=100000))
+  {
+   if(n%2!=0)
+   {
+   printf("ODD");
+   }
+   else
+   {
+   printf("EVEN");
+   }
+  }
+  else
+  {
+    printf("-1");
+  }
 }
